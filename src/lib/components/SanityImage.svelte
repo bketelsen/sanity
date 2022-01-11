4582266bc1723eb0fae094726f02cfd3f52e1477
Brian Ketelsen <bketelsen@gmail.com>
2022-01-11 11:22:36 +0000

    auto format image

--- src/lib/components/SanityImage.svelte
+++ src/lib/components/SanityImage.svelte
@@ -6,8 +6,7 @@
   export let alt = undefined
   export let classes
 
-  // Once loaded, the image will transition to full opacity
-  let loaded = false
+
 </script>
 
 {#if image}
@@ -16,7 +15,6 @@
     src={urlFor(image).width(maxWidth).fit('fillmax').auto('format')}
     alt={alt || image.alt || ''}
     class={classes}
-    style="aspect-ratio: {image.metadata.dimensions}; opacity: {loaded ? 1 : 0}; transition: .2s opacity;"
-    on:load={() => (loaded = true)}
+    style="aspect-ratio: {image.metadata.dimensions};"
   />
 {/if}
