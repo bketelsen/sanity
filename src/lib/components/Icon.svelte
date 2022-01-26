81dcb43066ea3e0a977be2201a4149f91697b94d
Brian Ketelsen <bketelsen@gmail.com>
2022-01-26 08:56:58 -0500

    footer icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -2,6 +2,11 @@
 	import Icon from 'svelte-icons-pack/Icon.svelte';
 	import Apple from 'svelte-icons-pack/fa/FaBrandsApple';
 	import Windows from 'svelte-icons-pack/fa/FaBrandsWindows';
+	import Twitter from 'svelte-icons-pack/fa/FaBrandsTwitter';
+	import LinkedIn from 'svelte-icons-pack/fa/FaBrandsLinkedin';
+	import Twitch	from 'svelte-icons-pack/fa/FaBrandsTwitch';
+	import Github from 'svelte-icons-pack/fa/FaBrandsGithub';
+	import Youtube from 'svelte-icons-pack/fa/FaBrandsYoutube';
 	import Cog from 'svelte-icons-pack/fa/FaSolidCog';
 
 	export let kind;
@@ -17,6 +22,21 @@
 			break;
 		case 'windows':
 			icon = Windows;
+			break;
+		case 'twitter':
+			icon = Twitter;
+			break;
+		case 'linkedin':
+			icon = LinkedIn;
+			break;
+		case 'twitch':
+			icon = Twitch;
+			break;
+		case 'github':
+			icon = Github;
+			break;
+		case 'youtube':
+			icon = Youtube;
 			break;
 		default:
 			icon = Cog;
