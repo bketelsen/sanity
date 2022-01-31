37a1fbd291a59b71040eac2397513b94dfe35a50
Brian Ketelsen <bketelsen@gmail.com>
2022-01-30 20:04:16 -0500

    card changes

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -3,50 +3,34 @@
 	import SanityImage from './SanityImage.svelte';
 </script>
 
-<div class="p-4 md:w-1/3">
-	<a sveltekit:prefetch href={`/blog/${post.slug}`}>
-		<div class="card">
-			<SanityImage
-				image={post.image.asset}
-				maxWidth={402}
-				alt={post.title}
-				classes="w-full object-contain object-center"
-			/>
-			<div class="p-6">
+<a sveltekit:prefetch href={`/blog/${post.slug}`}>
+	<div
+		class="mx-2 my-4 max-w-xs cursor-pointer  border border-gray-100 shadow-lg hover:shadow-2xl rounded p-4 hover:bg-azure-100"
+	>
+		<SanityImage
+			image={post.image.asset}
+			maxWidth={402}
+			alt={post.title}
+			classes="rounded object-cover w-full h-48"
+		/>
+		<div class="p-6">
+			<ul class="flex flex-wrap">
 				{#each post.categories || [] as cat}
-					<span class="category"><a href="/category/{cat.slug.current}">{cat.title}</a></span>
+					<li class="category"><a href="/category/{cat.slug.current}">{cat.title}</a></li>
 				{/each}
+
 				{#each post.technologies || [] as tech}
-					<span class="technology"><a href="/technology/{tech.slug.current}">{tech.title}</a></span>
+					<li class="technology"><a href="/technology/{tech.slug.current}">{tech.title}</a></li>
 				{/each}
-				<h1 class="text-xl mt-2 font-bold text-mix-600 mb-3">{post.title}</h1>
-				<p class="leading-relaxed mb-3">
-					{post.excerpt ? post.excerpt : ''}
-				</p>
 				{#each post.tags || [] as tag}
-					<span class="tag"><a href="/tag/{tag.slug.current}">#{tag.title}</a></span>
-				{/each}
-				<div class="mt-2 flex items-center flex-wrap ">
-					<a
-						sveltekit:prefetch
-						href={`/blog/${post.slug}`}
-						class="text-azure-700 inline-flex items-center md:mb-2 lg:mb-0"
-						>Read Now
-						<svg
-							class="w-4 h-4 ml-2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							stroke-width="2"
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						>
-							<path d="M5 12h14" />
-							<path d="M12 5l7 7-7 7" />
-						</svg>
-					</a>
-				</div>
-			</div>
+				<li class="tag"><a href="/tag/{tag.slug.current}">#{tag.title}</a></li>
+			{/each}
+			</ul>
+			<h1 class="font-bold text-xl md:text-2xl pt-6 pb-4">{post.title}</h1>
+			<p class="text-md text-gray-800 pb-2">
+				{post.excerpt ? post.excerpt : ''}
+			</p>
+
 		</div>
-	</a>
-</div>
+	</div>
+</a>
