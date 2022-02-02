776d241c83501c60f358675d661f2ed329281d69
Brian Ketelsen <bketelsen@gmail.com>
2022-02-02 04:57:55 -0500

    reduce max width

--- src/lib/components/CardGroup.svelte
+++ src/lib/components/CardGroup.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="py-4 md:py-8 lg:py-12 max-w-6xl mx-auto bg-base-100">
-	<div class="grid grid-cols-3 gap-4">
-	<slot/>
+	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+		<slot/>
 	</div>
 </div>
