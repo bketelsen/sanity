a8b4249e82f5a97a50f96421c45604b01ab0511a
Brian Ketelsen <bketelsen@gmail.com>
2022-02-04 12:14:00 -0500

    page changes

--- src/lib/components/SeoPage.svelte
+++ src/lib/components/SeoPage.svelte
@@ -3,6 +3,7 @@
 	export let global;
 	import SvelteSeo from 'svelte-seo';
 	let slug = page.slug.current;
+	console.log("slug:", slug)
 	let title = page.seo?.title || page.title;
 	title = title + ' | ' + global.siteName;
 </script>
@@ -17,7 +18,7 @@
 		type: 'website',
 		images: [
 			{
-				url: 'https://og-sooty.vercel.app/api/open-graph-image?path=/brian.dev' + page.slug.current,
+				url: 'https://og-sooty.vercel.app/api/open-graph-image?path=/brian.dev/page' + page.slug.current,
 				width: 850,
 				height: 650,
 				alt: page.title
