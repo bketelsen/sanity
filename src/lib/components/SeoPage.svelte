d14f9f855b985aa45d1a5521f94a07a967430e10
Brian Ketelsen <bketelsen@gmail.com>
2022-02-04 05:21:25 -0500

    seo changes

--- src/lib/components/SeoPage.svelte
+++ src/lib/components/SeoPage.svelte
@@ -1,30 +1,27 @@
 <script>
-    export let page;
-    export let global;
-    console.log(page)
-    import SvelteSeo from "svelte-seo";
-    let slug = page.slug.current === "home" ? "/" : page.slug.current;
-    let title = page.seo?.title || page.title;
-    title = title + " | " + global.siteName;
-
-
+	export let page;
+	export let global;
+	import SvelteSeo from 'svelte-seo';
+	let slug = page.slug.current;
+	let title = page.seo?.title || page.title;
+	title = title + ' | ' + global.siteName;
 </script>
 
 <SvelteSeo
-    title={title}
-    description= {page.seo?.metaDescription || page.description}
-  openGraph={{
-    title: page.seo?.metaTitle || page.title,
-    description: page.seo?.metaDescription || page.herotext,
-    url: global.siteUrl +  slug,
-    type: 'website',
-    images: [
-      {
-        url:  "https://og-sooty.vercel.app/api/open-graph-image?path=/brian.dev" + page.slug.current,
-        width: 850,
-        height: 650,
-        alt: page.title
-      }
-     ]
-  }}
+	{title}
+	description={page.seo?.metaDescription || page.description}
+	openGraph={{
+		title: page.seo?.metaTitle || page.title,
+		description: page.seo?.metaDescription || page.herotext,
+		url: global.siteUrl + slug,
+		type: 'website',
+		images: [
+			{
+				url: 'https://og-sooty.vercel.app/api/open-graph-image?path=/brian.dev' + page.slug.current,
+				width: 850,
+				height: 650,
+				alt: page.title
+			}
+		]
+	}}
 />
