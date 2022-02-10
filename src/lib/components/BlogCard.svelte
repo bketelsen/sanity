70a171bd06e914762a130ba61959852fdd861121
Brian Ketelsen <bketelsen@gmail.com>
2022-02-10 13:18:40 -0500

    make image backgrounds on cards

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -3,22 +3,20 @@
 	import SanityImage from './SanityImage.svelte';
 </script>
 
-<a sveltekit:prefetch href={`${post.slug}`}>
-	<div class="card text-center shadow-2xl">
-		<figure>
-			<SanityImage image={post.image} maxWidth={402} alt={post.title} classes="rounded-xl" />
-		</figure>
-		<div class="card-body">
-			<h2 class="card-title">{post.title}</h2>
-			<p>{post.excerpt}</p>
-			<div class="justify-center card-actions">
-				<button class="mt-5 btn btn-outline btn-primary">Read More</button>
-			</div>
-			<div class="justify-center card-actions">
-				{#each post.tags || [] as tag}
-					<div class="tag"><a href={tag.scopedSlug.current}>#{tag.taxonomy.title}</a></div>
-				{/each}
-			</div>
+<div class="card shadow-xl image-full">
+	<figure>
+		<SanityImage image={post.image} maxWidth={402} alt={post.title} classes="rounded-xl" />
+	</figure>
+	<div class="justify-end card-body">
+		<h2 class="card-title">{post.title}</h2>
+		<p>{post.excerpt}</p>
+		<div class="justify-center card-actions">
+			<button class="mt-5 btn btn-outline btn-accent">Read More</button>
+		</div>
+		<div class="justify-center card-actions">
+			{#each post.tags || [] as tag}
+				<div class="tag"><a href={tag.scopedSlug.current}>#{tag.taxonomy.title}</a></div>
+			{/each}
 		</div>
 	</div>
-</a>
+</div>
--- src/lib/components/RecentPosts.svelte
+++ src/lib/components/RecentPosts.svelte
@@ -7,9 +7,11 @@
 <section class="text-content-base body-font">
 	<div class="container mt-10 px-5 py-16 mx-auto">
 		<h2 class="sm:text-3xl text-2xl font-medium  title-font text-center  mb-10">Recent Posts</h2>
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+		<div class="flex flex-wrap -m-3">
 			{#each posts as post}
-				<BlogCard {post} />
+				<div class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+					<BlogCard {post} />
+				</div>
 			{/each}
 		</div>
 	</div>
